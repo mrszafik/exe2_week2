b9761fae1f11d68bb977654732d15122840a21eb
Maciej Szafrański <szafmac@gmail.com>
2017-11-20 21:09:40 +0100

    Add project files.

---
+++ exe2_week2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exe2_week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
